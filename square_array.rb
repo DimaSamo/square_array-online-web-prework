684ee2ec277f2efada43bbe12c69e16a82e9c2a6
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-06 02:26:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array=[]
   array.each do |element|
-    new_array.push(Math.sqrt(element))
+    new_array.push(element**2)
   end
   new_array
 end
